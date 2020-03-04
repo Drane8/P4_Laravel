8cbecbf4736570dc4ae0c5cd5a5aada1d5b47f4d
Dani <danigb92@hotmail.com>
2020-03-04 10:09:13 +0100

    Pasada funcion al controlador

--- app/Http/Controllers/InventarioController.php
+++ app/Http/Controllers/InventarioController.php
@@ -19,14 +19,41 @@ class InventarioController extends Controller
         $instalaciones = Instalacion::all();
         $articulos = Articulo::all();
         return view('insertar', ['inventarios' => $inventarios, 'instalaciones' => $instalaciones, 'articulos' => $articulos]);
+    }
 
-        
+    public function insertar(Request $request)
+    {
+        $aula = $request->get('aula');
+        $claveArticulo = explode("|", $request->get('articulo'));
+        $validator = Validator::make($request->all(), [
+            'aula' => 'required|unique:inventario,clave_instalacion,NULL,id,codigo_articulo,' . $claveArticulo[0],
+            'articulo' => 'required|unique:inventario,codigo_articulo,NULL,id,clave_instalacion,' . $aula,
+            'cantidadArticulos' => 'required|numeric|between:1,100',
+            'fecha' => 'nullable|before_or_equal:' . date('Y-m-d'),
+            'observaciones' => 'max:250'
+        ]);
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+        $task = new Inventario;
+        $task->clave_instalacion = $request->aula;
+        $articulo = explode("|", $request->articulo);
+        $task->codigo_articulo = $articulo[0];
+        $task->articulo = $articulo[1];
+        $task->cantidad = $request->cantidadArticulos;
+        $task->fecha_compra = $request->fecha;
+        $task->observaciones = $request->observaciones;
+        $task->save();
+        return redirect('/');
     }
 
     public function consultar()
     {
+        $inventarios = [];
         $instalaciones = Instalacion::all();
-        return view('consultar', ['instalaciones' => $instalaciones]);
+        return view('consultar', ['instalaciones' => $instalaciones, 'inventarios' => $inventarios]);
     }
 
     public function deleteInventario(Request $request)
--- routes/web.php
+++ routes/web.php
@@ -11,39 +11,17 @@
 |
 */
 
+use App\Http\Controllers\InventarioController;
 use App\Inventario;
-
-Route::get("/", 'InventarioController@index');
-Route::get("/consultar", 'InventarioController@consultar');
-
 use Illuminate\Http\Request;
 
-Route::post('/inventario', function (Request $request) {
-    $aula = $request->get('aula');
-    $claveArticulo = explode("|",$request->get('articulo'));
-    $validator = Validator::make($request->all(), [
-        'aula' => 'required|unique:inventario,clave_instalacion,NULL,id,codigo_articulo,' . $claveArticulo[0],
-        'articulo' => 'required|unique:inventario,codigo_articulo,NULL,id,clave_instalacion,' . $aula,
-        'cantidadArticulos' => 'required|numeric|between:1,100',
-        'fecha' => 'nullable|before_or_equal:' . date('Y-m-d'),
-        'observaciones' => 'max:250'
-    ]);
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-    $task = new Inventario;
-    $task->clave_instalacion = $request->aula;
-    $articulo = explode("|", $request->articulo);
-    $task->codigo_articulo = $articulo[0];
-    $task->articulo = $articulo[1];
-    $task->cantidad = $request->cantidadArticulos;
-    $task->fecha_compra = $request->fecha;
-    $task->observaciones = $request->observaciones;
-    $task->save();
-    return redirect('/');
-});
+Route::get("/", 'InventarioController@index');
+
+Route::post('/inventario', 'InventarioController@insertar');
+
+Route::get("/consultar", 'InventarioController@consultar');
+
+Route::post("/consultar",'InventarioController@consultar');
 
 Route::post('/inventario/borrarinventario', 'InventarioController@deleteInventario');
 
